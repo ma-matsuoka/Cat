0ae6d7c6cc009ef960c9f4433db5ba1a9684ee13
Matsuoka Masahiro <ma-matsuoka@tgl.co.jp>
2019-09-10 09:19:49 +0900

    Initial commit

---
+++ Example/Cat/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Cat
+//
+//  Created by Matsuoka Masahiro on 09/10/2019.
+//  Copyright (c) 2019 Matsuoka Masahiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
